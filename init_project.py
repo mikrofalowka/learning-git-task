296cbc1fa39611ae2961c35871e2d6faa135a31d
Jan Kuncewicz <jankuncewicz@gmail.com>
2024-07-16 19:46:14 +0200

    add print sumy init_project.py

--- init_project.py
+++ init_project.py
@@ -10,3 +10,4 @@ for sklep, artykul in lista_zakupow.items():
     print(f"Ide do {str(sklep).title()} i kupuje tam {str(artykul).title()}")
     ilosc_produktow = ilosc_produktow + len(lista_zakupow[sklep])
 
+print(f"W sumie kupuje {ilosc_produktow} produktow")
