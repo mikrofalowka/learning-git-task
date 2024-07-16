1d5da513679ae5dc5efeae3f84b15b5105185794
Jan Kuncewicz <jankuncewicz@gmail.com>
2024-07-16 19:45:44 +0200

    iteration init_project.py

--- init_project.py
+++ init_project.py
@@ -4,3 +4,9 @@ lista_zakupow = {
     "kwiaciarnia":["tulipany","ziemia","nawoz","pelargonia"]
 }
 
+ilosc_produktow = 0
+
+for sklep, artykul in lista_zakupow.items():
+    print(f"Ide do {str(sklep).title()} i kupuje tam {str(artykul).title()}")
+    ilosc_produktow = ilosc_produktow + len(lista_zakupow[sklep])
+
