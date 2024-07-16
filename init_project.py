89383195db68e5f3f8f1a52dc710170201652632
Jan Kuncewicz <jankuncewicz@gmail.com>
2024-07-16 19:39:21 +0200

    Add init_project.py

---
+++ init_project.py
@@ -0,0 +1,12 @@
+lista_zakupow = {
+    "warzywniak":["cukinia","baklazan","dynia"],
+    "miesny":["kurczak","boczek","szynka"],
+    "kwiaciarnia":["tulipany","ziemia","nawoz"]
+}
+ilosc_produktow = 0
+
+for sklep, artykul in lista_zakupow.items():
+    print(f"Ide do {str(sklep).title()} i kupuje tam {str(artykul).title()}")
+    ilosc_produktow = ilosc_produktow + len(lista_zakupow[sklep])
+
+print(f"W sumie kupuje {ilosc_produktow} produktow")
